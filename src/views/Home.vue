96ea33969e5e2b77410ed4c83074b8130c2741ba
liuyao <670652018@qq.com>
2018-10-25 21:26:41 +0800

    初始化目录结构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello world
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style lang="stylus" scoped>
+@import "~@/assets/stylus/variable"
+.home
+  color: $color-theme
+</style>
